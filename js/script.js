2926be98ea27d9421e692c24b15fcaea9bd513b2
Gonçalo Venâncio Xavier <2190788@my.ipleiria.pt>
2021-01-27 14:45:08 +0000

    Added Search functionaility on all pages

--- js/script.js
+++ js/script.js
@@ -1,25 +1,23 @@
 var cloneMedia = $('.media').clone();
 
 $('#btPesquisar').on('click', function(){
-
+    $('.row').html('');
     var valorPesquisa = $('#pesquisa').val();
 
     $('.media-list').html('');
 
-    $.ajax({
-        method: "GET",
-        url: "http://ws.audioscrobbler.com/2.0/?method=track.search&track=" + valorPesquisa + "&api_key=db577d25137963e669181d8a9eedac9c&format=json"
-    })
-        .done(function(msg){
-            msg.results.trackmatches.track.forEach(function(result){
-                var liMedia = cloneMedia.clone();
-                $('.title', liMedia).text(result.name);
-                $('.ano', liMedia).text(result.artist);
-                $('#detalhes', liMedia).attr('href', "detalhes.html?music="+result.name+"&artist="+result.artist);
-                getAlbumImage(result.name, result.artist, liMedia);
-                $('.media-list').append(liMedia);
-            })
-        })
+
+    var parameter = $("#parameter").val();
+
+    if(parameter == 1){
+        searchTrack(valorPesquisa);
+    }else if(parameter == 2){
+        searchArtist(valorPesquisa);
+    }
+    else{
+        searchAlbum(valorPesquisa);
+    }
+
 });
 
 $(document).ready(function() {
@@ -28,6 +26,7 @@ $(document).ready(function() {
         $('.media-list').html('');
         top10(null);
     }else if(body.is('.details')){
+        $('.media-list').html('');
         var urlParams = new URLSearchParams(window.location.search);
         var value = urlParams.get('music');
 
@@ -95,15 +94,19 @@ function detalhes(music, artist){
     })
         .done(function(msg){
             var song =  msg.track;
+            console.log(msg);
             $('#music').text(song.name);
-            $('#artist').text(artist);
+            $('#artist').text(song.artist.name);
             var image = $(".details").find('.image-album');
-
             if(song.album != null){
                 image.attr('src', song.album.image[3]['#text']);
-                $('#album-name').text(song.album.title);
+                $('#album-name').text("Album: " + song.album.title);
             }else{
                 image.attr('src', 'images/no_image.png');
+            }
+
+            if(song.wiki != null){
+                $('#wiki').text(song.wiki.content);
             }
         })
 }
@@ -129,4 +132,55 @@ function addFav(){
     if($('body').is('.top10')){
 
     }
+}
+
+function searchTrack(search){
+    $.ajax({
+        method: "GET",
+        url: "http://ws.audioscrobbler.com/2.0/?method=track.search&track=" + search + "&api_key=db577d25137963e669181d8a9eedac9c&format=json"
+    })
+        .done(function(msg){
+            msg.results.trackmatches.track.forEach(function(result){
+                var liMedia = cloneMedia.clone();
+                $('.title', liMedia).text(result.name);
+                $('.ano', liMedia).text(result.artist);
+                $('#detalhes', liMedia).attr('href', "detalhes.html?music="+result.name+"&artist="+result.artist);
+                getAlbumImage(result.name, result.artist, liMedia);
+                $('.media-list').append(liMedia);
+            })
+        })
+}
+
+function searchArtist(search){
+    $.ajax({
+        method: "GET",
+        url: "http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=" + search + "&api_key=db577d25137963e669181d8a9eedac9c&format=json"
+    })
+        .done(function(msg){
+            msg.results.artistmatches.artist.forEach(function(result){
+                var liMedia = cloneMedia.clone();
+                $('.title', liMedia).text(result.name);
+                $('.ano', liMedia).text(result.artist);
+                //$('#detalhes', liMedia).attr('href', "detalhes.html?music="+result.name+"&artist="+result.artist);
+                //getAlbumImage(result.name, result.artist, liMedia);
+                $('.media-list').append(liMedia);
+            })
+        })
+}
+
+function searchAlbum(search){
+    $.ajax({
+        method: "GET",
+        url: "http://ws.audioscrobbler.com/2.0/?method=album.search&album="+ search +"&api_key=db577d25137963e669181d8a9eedac9c&format=json"
+    })
+        .done(function(msg){
+            msg.results.albummatches.album.forEach(function(result){
+                var liMedia = cloneMedia.clone();
+                $('.title', liMedia).text(result.name);
+                $('.ano', liMedia).text(result.artist);
+                //$('#detalhes', liMedia).attr('href', "detalhes.html?music="+result.name+"&artist="+result.artist);
+                getAlbumImage(result.name, result.artist, liMedia);
+                $('.media-list').append(liMedia);
+            })
+        })
 }
